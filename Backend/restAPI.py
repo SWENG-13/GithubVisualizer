63d0041d62336ffdd80f8cb0cae7402cf3bd3265
Kajus1331 <krisciuk@tcd.ie>
2022-11-20 13:42:56 +0000

    Added contributors stats. Needs a bug fix

--- Backend/restAPI.py
+++ Backend/restAPI.py
@@ -1,7 +1,7 @@
 # pip -install requests
 
-import requests;
-import json;
+import requests
+import json
 
 # Method to request data from the api
 def call_url(url, jsonName, headers, isAuth):
@@ -20,7 +20,7 @@ token = ""
 #token = ""     
 headers = {'Authorization': 'token ' + token}
 #url = 'https://api.github.com/users/HarveyBrezinaConniffe/repos'
-url = 'https://api.github.com/users/matthu2711/repos'
+#url = 'https://api.github.com/users/matthu2711/repos'
 url2 = 'https://api.github.com/repos/matthu2711/{}/stats/code_frequency'
 #url = 'https://api.github.com/users/Kajus1331/repos'
 #url2 = 'https://api.github.com/repos/Kajus1331/AlgsAssignmentFinalProject/stats/code_frequency'
@@ -37,16 +37,16 @@ url2 = 'https://api.github.com/repos/matthu2711/{}/stats/code_frequency'
 
 
 # REPOS
-call_url(url, "repos", headers, True)
+#call_url(url, "repos", headers, True)
 f = open('repos.json')   # Opening JSON file
-data = json.load(f) # returns JSON object as a dictionary
-length = (len(data))
+repoData = json.load(f) # returns JSON object as a dictionary
+repoLength = (len(repoData))
 repos = []
 isRepoEmpty = []
 
-for i in range(0, length):
-  repos.append(data[i]["name"])
-  if data[i]["size"] > 0:
+for i in range(0, repoLength):
+  repos.append(repoData[i]["name"])
+  if repoData[i]["size"] > 0:
     isRepoEmpty.append(False)
   else:
     isRepoEmpty.append(True)  
@@ -56,14 +56,14 @@ for i in range(0, length):
 
 freqList = [] # Data is appended in the form [time, additions, subtractions]
 
-for j in range(0, length):
+for j in range(0, repoLength):
   repoName = repos[j]
   url3 = url2.format(repoName)
   listTmp = []
   if isRepoEmpty[j]:
     listTmp.append("0")
   else:
-    call_url(url3, "reposFreq", headers, True)
+    #call_url(url3, "reposFreq", headers, True)
     f = open('reposFreq.json')
     data = json.load(f)
     length2 = (len(data))
@@ -79,13 +79,60 @@ totalCommits = 0
 
 for i in repos:
   url = "https://api.github.com/repos/matthu2711/{}/commits".format(i)
-  call_url(url, "commits", headers, True)
+  #call_url(url, "commits", headers, True)
   f = open('commits.json')
   data = json.load(f)
-  length = (len(data))
-  commits.append(length)
-  totalCommits = totalCommits + length
+  length2 = (len(data))
+  commits.append(length2)
+  totalCommits = totalCommits + length2
 
-print(totalCommits)
+#print(totalCommits)
+
+#----------------------- THIS IS NOT CURRENTLY WORKING DONT CHANGE!!!!!!!!------------------------------------
+#-----------------------------------------------------------------------------------------------------
+
+
+
+# CONTRIBUTORS
+#url = "https://api.github.com/repos/matthu2711/{}/stats/contributors"
+#url7 = "https://api.github.com/repos/matthu2711/calcWebApp/contributors"
+#url5 = "https://api.github.com/repos/matthu2711/calcWebApp/stats/contributors"
+#call_url(url5, "contributorStats", headers, True)
+
+contributorsList = []   # Data is appended in the form [User, #added, #subtracted, #contributions]
+
+for j in range(0, -1):
+  print(j)
+  repoName = repos[j]
+  url1 = url.format(repoName)
+  print(url1)
+  listTmp = []
+  if isRepoEmpty[j]:
+    listTmp.append("null")
+  elif str(repoData[j]["fork"]) == "True":
+    listTmp.append("null")
+  else:
+    #call_url(url1, "contributorStats", headers, True)
+    f = open('contributorStats.json')
+    data = json.load(f)
+    length2 = (len(data))
+    weeks = (len(data[0]["weeks"]))
+    listTmp.append(repoName)
+    for i in range(0, length2):
+      totalAdd = 0
+      totalSub = 0
+      listTmp.append(''.join(str(data[i]["author"]["login"])))
+      for k in range(0, weeks):
+        totalAdd = totalAdd + (data[i]["weeks"][k]["a"])
+        totalSub = totalSub + (data[i]["weeks"][k]["d"])
+      listTmp.append(totalAdd)
+      listTmp.append(totalSub)
+      listTmp.append(''.join(str(data[i]["total"])))
+  contributorsList.append(listTmp)  
+
+#print(contributorsList)  
+
+#-------------------------------------DONT CHANGE ANYTHING ABOVE--------------------------
+
 # Closing file
 f.close()
