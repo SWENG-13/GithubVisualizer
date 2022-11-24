300db73efac023ed66449c40266eb4b6427edd8b
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-11-24 14:49:00 +0000

    Format Kajus' backend code

--- dataloader.py
+++ dataloader.py
@@ -1,89 +1,92 @@
-# pip -install requests
-
 import requests
 import json
 
-repoLength = 0
-repos = []
-isRepoEmpty = []
-freqList = [] # Data is appended in the form [time, additions, subtractions]
-commits = []
-totalCommits = 0
-repoData = []
-contributorsList = []   # Data is appended in the form [User, #added, #subtracted, #contributions]
-userInfoList = []
-loadUser = []
-userData = []
-
-
-# Method to request data from the api
-def call_url(url, jsonName, headers, isAuth):
-  if isAuth == True:
-    response = requests.get(url, headers = headers)
+# Function that returns information about a given user
+# An access token can optionally be supplied
+def getUserInfo(username, token=None):
+  # Add token to headers if supplied
+  headers = {}
+  if token is not None:
+    headers["Authorization"] = "token {}".format(token)
+  requestURL = 'https://api.github.com/users/{}'.format(username)
+  response = requests.get(requestURL) 
+  if not response.ok:
+    return {"error": True, "error_details": response.json()}
   else:
-    response = requests.get(url)
-  response_dict = response.json()
-  with open('{}.json'.format(jsonName), 'w', encoding='utf-8') as f:
-    json.dump(response_dict, f, ensure_ascii=False, indent=4)
+    output = response.json()
+    output["error"] = False
+    return output
 
-def getUserInfo(headers, url):
-  global loadUser, userData
-  call_url(url, "user", headers, True)
-  f = open('user.json')
-  loadUser = json.load(f)
-  userData.append(''.join(str(loadUser["login"])))
-  userData.append(''.join(str(loadUser["followers"])))
-  userData.append(''.join(str(loadUser["name"])))
-  userData.append(''.join(str(loadUser["following"])))
-  userData.append(''.join(str(loadUser["public_repos"])))
-  userData.append(''.join(str(loadUser["bio"])))
-  userData.append(''.join(str(loadUser["location"])))
-  userData.append(''.join(str(loadUser["avatar_url"])))
-  print(userData)
+# Function that returns the names of nonempty repos of a given user
+# An access token can optionally be supplied
+def getRepos(username, token=None):
+  # Add token to headers if supplied
+  headers = {}
+  if token is not None:
+    headers["Authorization"] = "token {}".format(token)
+  requestURL = 'https://api.github.com/users/{}/repos'.format(username)
+  response = requests.get(requestURL) 
+  if not response.ok:
+    return {"error": True, "error_details": response.json()}
+  else:
+    output = {"error": False, "repo_names": []}
+    data = response.json()
+    for repo in data:
+      # Check that repo is not empty
+      if repo["size"] > 0:
+        output["repo_names"].append(repo["name"])
+    # Return repo names
+    return output
+  
+# Function that returns the commit frequency of each of a users repos
+# Repo name must be in the format {OWNER}/{NAME}, e.g. SWENG-13/GithubVisualizer
+# An access token can optionally be supplied
+def getRepoFreq(username, token=None):
+  repos = getRepos(username, token=None)
+  # Add token to headers if supplied
+  headers = {}
+  if token is not None:
+    headers["Authorization"] = "token {}".format(token)
+  output = {"error": False, "commits": {}}
+  for repo in repos["repo_names"]:
+    requestURL = 'https://api.github.com/repos/{}/{}/stats/code_frequency'.format(username, repo)
+    # Max number of retries
+    maxRetries = 10
+    # Timeout between retries
+    retryTimeout = 0.1
+    responseCode = 202
+    attempts = 0
+    while responseCode == 202 and attempts < maxRetries:
+      response = requests.get(requestURL) 
+      responseCode = response.status_code
+      if responseCode == 202:
+        continue
+      if not response.ok:
+        return {"error": True, "error_details": response.json()}
+      else:
+        commits = response.json()
+        output["commits"][repo] = commits
+  return output
 
-
-
-def getRepos(headers, url):
-  global repos, repoLength, repoData, isRepoEmpty
-  call_url(url, "repos", headers, True)
-  f = open('repos.json')   # Opening JSON file
-  repoData = json.load(f) # returns JSON object as a dictionary
-  repoLength = (len(repoData))
-  for i in range(0, repoLength):
-    repos.append(repoData[i]["name"])
-    if repoData[i]["size"] > 0:
-      isRepoEmpty.append(False)
+# Function that gets the number of commits made by a user to all repos over all time
+# An access token can optionally be supplied
+def getTotalCommits(username, token=None):
+  repos = getRepos(username, token=None)
+  # Add token to headers if supplied
+  headers = {}
+  if token is not None:
+    headers["Authorization"] = "token {}".format(token)
+  output = {"error": False, "total_commits": 0}
+  for repo in repos["repo_names"]:
+    requestURL = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
+    response = requests.get(requestURL) 
+    if not response.ok:
+      return {"error": True, "error_details": response.json()}
     else:
-      isRepoEmpty.append(True)  
+      output["total_commits"] += len(response.json())
+  return output
 
-def getRepoFreq(headers, url2):
-  global freqList
-  for j in range(0, repoLength):
-    repoName = repos[j]
-    url3 = url2.format(repoName)
-    listTmp = []
-    if isRepoEmpty[j]:
-      listTmp.append("0")
-    else:
-      call_url(url3, "reposFreq", headers, True)
-      f = open('reposFreq.json')
-      data = json.load(f)
-      length2 = (len(data))
-      for i in range(0, length2):
-        listTmp.append(''.join(str(data[i])))
-    freqList.append(listTmp)    
-
-def getCommits(headers):
-  global commits, totalCommits
-  for i in repos:
-    url = "https://api.github.com/repos/matthu2711/{}/commits".format(i)
-    call_url(url, "commits", headers, True)
-    f = open('commits.json')
-    data = json.load(f)
-    length2 = (len(data))
-    commits.append(length2)
-    totalCommits = totalCommits + length2
-
+"""
 def getContributorData(headers, url):
   global contributorsList
   print(repoLength)
@@ -118,44 +121,4 @@ def getContributorData(headers, url):
         listTmp.append(totalSub)
         listTmp.append(''.join(str(data[i]["total"])))
     contributorsList.append(listTmp)  
-
-    
-# Used for testing/ ----------------CHANGE THEM WHEN TESTING
-token = "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"     
-headers = {'Authorization': 'token ' + token}
-urlUser = 'https://api.github.com/users/matthu2711'
-urlRepo = 'https://api.github.com/users/matthu2711/repos'
-urlFreq = 'https://api.github.com/repos/matthu2711/{}/stats/code_frequency'
-urlContr = 'https://api.github.com/repos/matthu2711/{}/stats/contributors'
-
-# Create an API request
-#isAuth = True
-#user = input("Enter the name of the user: ")
-#url = "https://api.github.com/users/{}/repos".format(User)
-#token = input("Enter auth token for more data: ")
-#if token is None:
-#  isAuth = False
-#else:
-#  headers = {'Authorization': 'token ' + token}
-
-
-# GET USER INFO
-#getUserInfo(headers, urlUser)
-
-# GET REPOS
-getRepos(headers, urlRepo)
-#print(repos)
-#print(repoLength)
-
-# REPO FREQ
-#getRepoFreq(headers, urlFreq)
-#print(freqList)
-#print(repoData)
-
-# COMMITS
-#getCommits(headers)
-#print(totalCommits)
-
-# CONTRIBUTORS
-getContributorData(headers, urlContr)
-print(contributorsList)
+    """
