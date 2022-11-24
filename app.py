817f9c84037f4afb457afd4829a98819c6b877c4
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-11-24 16:20:38 +0000

    Integrate frontend with backend.

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import plotly.graph_objects as go # charts
 from github import Github # tokens
 from datetime import datetime
 from collections import Counter # count
+import dataloader
+
+TOKEN = None
 
 ## function for getting and displaying profile information
     # user data: avatar (?), username, name, description, location
@@ -27,64 +30,54 @@ debug_desc = "hi"
 debug_location = "Dublin, Ireland"
 valid = True
 
-# read token
-file = open("token.txt")
-token = file.read()
-file.close()
-git = Github(token)
- 
-
 st.title("📈Github Data Visualisation")
 st.subheader="Measuring Software Engineering"
 
-user_in = st.text_input("Enter Github Username:")
-try:
-    pass # USER INPUT USERNAME AND FIND FROM API
-except Exception as e:
-        st.write("User not found")
-        valid = False
-    
-if user_in.__eq__("vicky-emuk"): # debug
-    
+user_in = None
+
+st.session_state.user_details = None
+def getUserInfo():
+  st.session_state.user_details = dataloader.getUserInfo(st.session_state.username, token=TOKEN)
+  if st.session_state.user_details["error"] == True:
+    st.session_state.user_details = None
+    st.write("User not found")
+  else:
     i1_1, i1_2, i1_3 = st.columns(3) # add optional column for avatar 
 
     with i1_1:
         st.markdown("""
                     **Username**\n
                     """)
-        st.write(debug_username) # (user_info["name"]) e.g as a way to call user info from list
+        st.write(st.session_state.username) # (user_info["name"]) e.g as a way to call user info from list
         st.markdown("""
                     **Name**\n
                     """)
-        st.write(debug_name)
+        st.write(st.session_state.user_details["name"])
         st.markdown("""
                     **Description**\n
                     """)
-        st.write(debug_desc)
+        st.write(st.session_state.user_details["bio"])
         st.markdown("""
                     **Location**\n
                     """)
-        st.write(debug_location)
+        st.write(st.session_state.user_details["location"])
         
     with i1_2:
         st.markdown("""
                     **Followers**\n
                     """)
-        st.write("4") # (user_info["name"]) e.g as a way to call user info from list
+        st.write(st.session_state.user_details["followers"]) # (user_info["name"]) e.g as a way to call user info from list
         st.markdown("""
                     **Following**\n
                     """)
-        st.write("2")
+        st.write(st.session_state.user_details["following"])
         
     with i1_3:
         st.markdown("""
-                    **Stars Recieved**\n
-                    """)
-        st.write("3")
-        st.markdown("""
                     **Public Repos**\n
                     """)
-        st.write("2")
-    
+        st.write(st.session_state.user_details["public_repos"])
+
     i2_1, i2_2 = st.columns(2)
-    
+
+st.text_input("Enter Github Username:", key='username', on_change=getUserInfo)
