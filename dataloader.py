290789baf38940c5feb62fc875323234be507231
Kajus1331 <krisciuk@tcd.ie>
2022-12-02 12:53:44 +0000

    Added in getRepoStats function

--- dataloader.py
+++ dataloader.py
@@ -25,7 +25,7 @@ def getRepos(username, token=None):
   if token is not None:
     headers["Authorization"] = "token {}".format(token)
   requestURL = 'https://api.github.com/users/{}/repos'.format(username)
-  response = requests.get(requestURL) 
+  response = requests.get(requestURL, headers = headers) 
   if not response.ok:
     return {"error": True, "error_details": response.json()}
   else:
@@ -57,7 +57,7 @@ def getRepoFreq(username, token=None):
     responseCode = 202
     attempts = 0
     while responseCode == 202 and attempts < maxRetries:
-      response = requests.get(requestURL) 
+      response = requests.get(requestURL, headers = headers) 
       responseCode = response.status_code
       if responseCode == 202:
         continue
@@ -68,14 +68,24 @@ def getRepoFreq(username, token=None):
         output["commits"][repo] = commits
   return output
 
-# Function that gets the number of commits made by a user to all repos over all time
+# Function that gets the number of commits made by a user to either all repos over all time
+# or a selected repo
 # An access token can optionally be supplied
-def getTotalCommits(username, token=None):
-  repos = getRepos(username, token=None)
-  # Add token to headers if supplied
+def getTotalCommits(username, token=None, givenRepo=None):
   headers = {}
   if token is not None:
     headers["Authorization"] = "token {}".format(token)
+  if givenRepo is not None:
+    output = {}
+    requestURL = "https://api.github.com/repos/{}/{}/commits".format(username, givenRepo)
+    response = requests.get(requestURL, headers = headers) 
+    if not response.ok:
+      return {"error": True, "error_details": response.json()}
+    else:
+      output["total_commits"] = len(response.json())
+    return output
+  repos = getRepos(username, token=None)
+  # Add token to headers if supplied
   output = {"error": False, "total_commits": 0}
   for repo in repos["repo_names"]:
     requestURL = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
@@ -106,6 +116,61 @@ def getRepoLanguages(username, token=None):
         else:
           output["languages"][key] = data[key]
   return output
+
+# Function that gets the contributors for any selected repo
+# An access token can optionally be supplied
+def getContributors(username, token=None, repo=None):
+  headers = {}
+  if token is not None:
+    headers["Authorization"] = "token {}".format(token)
+  output = {}  
+  requestURL = 'https://api.github.com/repos/{}/{}/contributors'.format(username, repo)
+  response = requests.get(requestURL, headers = headers)
+  if not response.ok:
+    return {"error": True, "error_details": response.json()}
+  else:  
+    data = response.json()
+    for i in range(0,len(data)):
+      output[i] = data[i]["login"]
+  return output  
+
+# Function that gets the repo stats for all repos over all time
+# An access token can optionally be supplied
+def getRepoStats(username, token):
+  repos = getRepos(username, token)
+  # Add token to headers if supplied
+  headers = {}
+  if token is not None:
+    headers["Authorization"] = "token {}".format(token)
+  output = {"error" : {"error" : "false"}}  
+  output1 = {}
+  commitsResult = getRepoFreq(username, token)
+  for repo in repos["repo_names"]:
+    requestURL = 'https://api.github.com/repos/{}/{}'.format(username, repo)
+    response = requests.get(requestURL, headers = headers)
+    if not response.ok:
+      return {"error": True, "error_details": response.json()}
+    else:
+      data = response.json()
+      output1 = {}
+      for key in data.keys():
+        if key == "name":
+          output1[key] = data[key]
+        if key == "owner":  
+          output1[key] = data[key]["login"]
+          output1["stars"] = "0"
+        if key == "forks_count":
+          output1[key] = data[key]
+        if key == "created_at":
+          output1[key] = data[key]
+        if key == "commits_url":
+          tmp = commitsResult["commits"][repo]
+          output1["commits"] = tmp
+        if key == "contributors_url":
+          result = getContributors(username, token, repo)
+          output1["contributors"] = result
+    output[repo] = output1      
+  return output  
 
 """
 def getContributorData(headers, url):
